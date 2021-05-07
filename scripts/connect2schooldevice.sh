161cc5aa657a458c400cc41c8f7c218367a24eab
codekoch <45600398+codekoch@users.noreply.github.com>
2021-05-07 23:53:29 +0200

    Update connect2schooldevice.sh

--- scripts/connect2schooldevice.sh
+++ scripts/connect2schooldevice.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-ipaddress=$(zenity --width=400 --entry --title "IP ADDRESS of schooldevice to connecto to:" --text "Format: xxx.xxx.xxx.xxx")
+ipaddress=$(zenity --width=400 --entry --title "IP-ADRESSE des Schulsystems:" --text "Format: xxx.xxx.xxx.xxx")
 exo-open --launch WebBrowser "http://$ipaddress:8080/guacamole"
