18d9b79a8a204afbd9799f4691d9d8820c03c43a
codekoch <45600398+codekoch@users.noreply.github.com>
2021-05-08 10:36:38 +0200

    Update loadSession.sh

--- scripts/loadSession.sh
+++ scripts/loadSession.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-su user0 -c 'zenity --info --width 300 --text "Please close all running aplications...\nAnd insert an external device with FAT32 filesystem" --title "ATTENTION!"' 
-path=`su user0 -c 'zenity --file-selection --directory --title="Choose folder with session backup" --filename=/media/user0/'` 
+su user0 -c 'zenity --info --width 300 --text "Bitte beende alle laufenden Anwendungen...\nund schließe ein externes Medium im FAT32 Format an" --title "ACHTUNG!"' 
+path=`su user0 -c 'zenity --file-selection --directory --title="Wähle das Verzeichnis mit dem Backup des Nutzerkontos" --filename=/media/user0/'` 
 if [[ $path =~ /media/user0/*/ ]];then
  cd /
  wget https://raw.githubusercontent.com/codekoch/schooldevice/master/scripts/rsync-homedir-local.txt -O rsync-homedir-local.txt
  chmod 777 rsync-homedir-local.txt
- rsync -a  --exclude-from=rsync-homedir-local.txt --progress $path/ /home/user0/ --delete | su user0 -c 'zenity --title "Session" --text "Loading from '$path'.\nDepending on the amount of data to be synchronized, it may take some time.\nPlease wait for Ok Button..." --progress --pulsate --auto-kill'
+ rsync -a  --exclude-from=rsync-homedir-local.txt --progress $path/ /home/user0/ --delete | su user0 -c 'zenity --title "Session" --text "wird wiederhergestellt von '$path'.\nJe nach Datenvolumen und Transfergeschwindigkeit kann dies etwas dauern.\nBitte warte auf den Ok Knopf..." --progress --pulsate --auto-kill'
 else
- su user0 -c 'zenity --info --width 300 --text "You choose no directory on external device" --title "ERROR!"'
+ su user0 -c 'zenity --info --width 300 --text "Du hast kein Verzeichnis auf dem externen Medium ausgewählt" --title "ERROR!"'
 fi
