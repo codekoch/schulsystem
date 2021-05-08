e78c70fc8f7b384c7d904a654b7a30a61fe08c1e
codekoch <45600398+codekoch@users.noreply.github.com>
2021-05-08 11:45:01 +0200

    Update connect2schulsystem.sh

--- scripts/connect2schulsystem.sh
+++ scripts/connect2schulsystem.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 ipaddress=$(zenity --width=400 --entry --title "IP-ADRESSE des entfernten Schulsystems:" --text "Format: xxx.xxx.xxx.xxx")
-notify-send -u critical " " "Drücke auf Anmelden (Bentuzername und Passwort sind nicht nötig)" 
+notify-send -u critical " " "Drücke auf Anmelden\n(Bentuzername und Passwort sind nicht nötig)" 
 exo-open --launch WebBrowser "http://$ipaddress:8080/guacamole"
