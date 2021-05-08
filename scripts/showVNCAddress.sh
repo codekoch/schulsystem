da062bfd5c560fc9cec537d986d15c8a398f024b
codekoch <45600398+codekoch@users.noreply.github.com>
2021-05-08 10:39:50 +0200

    Update showVNCAddress.sh

--- scripts/showVNCAddress.sh
+++ scripts/showVNCAddress.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 ipadress=`hostname -I | awk '{print $1}'`
 notify-send -u critical "Deine IP-ADRESSE:" "$ipadress" 
+notify-send -u critical "Wähle ACCEPT CONNECTIONS und drücke auf OK, um andere Verbindungen zuzulassen" 
