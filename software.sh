ad312d8d4959669d37ba1acc808a211e3e757329
codekoch <45600398+codekoch@users.noreply.github.com>
2023-04-18 08:39:19 +0200

    Update software.sh

--- software.sh
+++ software.sh
@@ -78,7 +78,7 @@ sudo apt install -y ./google-chrome-stable_current_amd64.deb
 #### Edge
 sudo apt install -y software-properties-common apt-transport-https wget
 wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main"
+sudo add-apt-repository -y "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main"
 sudo apt install -y microsoft-edge-dev
 
 #### Openboard
