765d63d15e2b9bb21c598656d91e27361bfae5b1
codekoch <45600398+codekoch@users.noreply.github.com>
2024-12-09 11:58:48 +0100

    Update individualFix.sh

--- individualFix.sh
+++ individualFix.sh
@@ -18,20 +18,14 @@ echo -e "\\033[34;1m${@}\033[0m"
 
 
 #### individuelle Veränderungen:
-fixversion='Bobdue'
+fixversion='Greenfoot'
 yellow_msg "Installiere die individuellen Veränderungen (Fix $fixversion)..."
 sudo echo 'notify-send -t 5000 "+Fix" "'$fixversion'"' >> /usr/bin/setbackground.sh
 ########
 ######## ab hier kommt der Code der für die individuellen Veränderungen sorgt
 ########
-sudo apt-get install libboost-all-dev libwxgtk3.2-dev libusb-1.0-0-dev bison++ flex libfl-dev make gcc g++ libcanberra-gtk-module libcanberra-gtk3-module
-wget "https://downloads.bob3.org/bobdude/bobdude-1.5.0.tar.bz2"
-tar -xjf bobdude-1.5.0.tar.bz2
-cd bobdude-1.5.0
-./configure
-make
-sudo make install
-cd ..
+wget https://www.greenfoot.org/download/files/Greenfoot-linux-x64-390.deb
+sudo apt install -y ./Greenfoot-linux-x64-390.deb
 
 
 ########
